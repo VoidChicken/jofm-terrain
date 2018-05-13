eacca3b3840a9a0483a1afbc83634cf00ab54280
Void Chicken <acronstudios@gmail.com>
2018-05-12 22:29:13 -0700

    Added basic procedural map and resized grass sprite to not have extra space at top.

---
+++ Assets/VoidCode/Stuff/Terrain/Scripts/VoidTerrain.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Dis code was written by 
+ * 
+ * Void Chicken
+ * 
+ * Give credit plz
+ * 
+ * Much tanks
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace VoidChicken.Terrain
+{
+    public class VoidTerrain : MonoBehaviour
+    {
+
+        public GameObject terrainBlock;
+
+        public int width = 0;
+
+        public float noiseScale = 0f;
+        public float noiseAmplitude = 0f;
+        public AnimationCurve noiseRemap;
+
+        private int[] heights;
+
+
+        public float blockHeight = 0f;
+
+
+
+        float GenerateHeight(float x)
+        {
+            return Mathf.Floor(noiseRemap.Evaluate(Mathf.PerlinNoise(x * noiseAmplitude, 0)) * noiseScale);
+        }
+
+        public void Generate()
+        {
+            heights = new int[width];
+
+            for (int i = 0; i < width; i++)
+            {
+                int height = (int)GenerateHeight(i);
+                for (int j = 0; j < height; j++) {
+                    Instantiate(terrainBlock, new Vector3(i, j * blockHeight, 0), Quaternion.identity, transform);
+                }
+            }
+        }
+
+    }
+}
